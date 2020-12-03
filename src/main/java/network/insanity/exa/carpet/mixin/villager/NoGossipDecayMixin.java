e282c9a69e23cfcab700c0d1e198f7d903689e34
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-12-03 11:38:40 +0100

    temporarily disable all mixins

--- src/main/java/network/insanity/exa/carpet/mixin/villager/NoGossipDecayMixin.java
+++ src/main/java/network/insanity/exa/carpet/mixin/villager/NoGossipDecayMixin.java
@@ -2,6 +2,9 @@ package network.insanity.exa.carpet.mixin.villager;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AbstractTraderEntity;
@@ -22,5 +25,11 @@ public abstract class NoGossipDecayMixin extends AbstractTraderEntity {
         if(this.getHeadRollingTimeLeft() > 0) {
             this.setHeadRollingTimeLeft(this.getHeadRollingTimeLeft() - 1);
         }
+    }
+
+    @Inject(method = "decayGossip()V", at = @At("HEAD"))
+    public void cancelDecayGossip(CallbackInfo ci)
+    {
+        ci.cancel();
     }
 }
